f11c23428948a1e16e5296871c9551f6a739fb05
Deciduous <34228330+deciduousl@users.noreply.github.com>
2020-05-15 00:56:56 +0800

    Update 1688_getimg.py
    更新了 sku图片下载时 将sku图片的文字说明直接存储为图片名

--- 1688_getimg.py
+++ 1688_getimg.py
@@ -14,14 +14,18 @@ num_xq = 0
 # url 下载图片地址
 # num 图片张数计数  第一张 命名为 1_01 第二张 1_02 方便淘宝后台上传时按文件名排序不会错乱
 # filename 文件夹名字
-def download_img(url,num,filename): #下载图片 方法
+# sku_name sku单个图片名称
+def download_img(url,num,filename,sku_name): #下载图片 方法
     d = 'D:\program\python_project\\1688\\'+filename+'\\'
     print(d)
     if (num<10):
         num = '0'+str(num)
     else:
         num = str(num)
-    path = d + '1_'+num+'.jpg'
+    if sku_name == '':
+        path = d + '1_'+num+'.jpg'
+    else:
+        path = d + sku_name+'.jpg'
     try:
         if not os.path.exists(d):
             os.mkdir(d)
@@ -57,29 +61,31 @@ if __name__ == '__main__':
 
 # print(txt)
 # 读取txt文本内容 结束
-    #     主图正则匹配
+#         主图正则匹配
     results = re.findall('"original":"(http.*?jpg)',txt)
     # print(results[0])
     for res in  results:
         num_zhutu = num_zhutu + 1
-        download_img(res,num_zhutu,'zhutu')
-        time.sleep(2)
+        download_img(res,num_zhutu,'zhutu','')
+        # time.sleep(1)
         print(res)
 
     # sku图片正则匹配
     results2 = re.findall('"imageUrl":"(http.*?jpg)', txt)
-    # print(results2[0])
-    for res2 in  results2:
+    results_alt = re.findall('name":"(.*?)"', txt)
+    print(len(results2))
+    for i in  range(len(results2)):
         num_sku = num_sku + 1
-        download_img(res2,num_sku,'sku')
-        time.sleep(2)
-        print(res2)
+
+        download_img(results2[i],num_sku,'sku',results_alt[i])
+        # time.sleep(1)
+        # print(results2[i])
 
     # 详情图匹配
     results3 = re.findall('<img.*?src=\\\\\"(http.*?\.jpg)', xqimg_txt)
     # print(results3[0])
     for res3 in results3:
         num_xq = num_xq + 1
-        download_img(res3, num_xq,'xq')
-        time.sleep(2)
-        print(res3)
+        download_img(res3, num_xq,'xq','')
+        # time.sleep(1)
+        # print(res3)
